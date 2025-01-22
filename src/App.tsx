e77c5fa6318550e775bb444730023f4cbcfc651b
hisuic <henryharlay@gmail.com>
2025-01-23 00:00:40 +0900

    add: with basic functions

---
+++ src/App.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import Graph from './Graph.tsx'
+import { graphRelations, dijkstra } from "./Graph.tsx";
+import './App.css';
+
+function App() {
+
+  const images = Object.fromEntries(
+    Object.entries(import.meta.glob('./assets/*.png', { eager: true }))
+      .map(([path, module]) => [path.replace('./assets/', ''), (module as any).default])
+  );
+
+  const keiroImages = Object.fromEntries(
+    Object.entries(import.meta.glob('./assets/keiro/*.png', { eager: true }))
+      .map(([path, module]) => [path.replace('./assets/keiro/*.png', ''), (module as any).default])
+  );
+
+  const buildingSelection = [
+    { value: '1', label: 'S-Port' },
+    { value: '2', label: '北会館・北館食堂' },
+    { value: '3', label: '南会館・南館食堂' },
+    { value: '4', label: '共通講義棟' },
+    { value: '5', label: '情報学部1号館' },
+    { value: '6', label: '情報学部2号館' },
+    { value: '7', label: '総合研究棟' },
+    { value: '8', label: '運動場' },
+    { value: '3', label: '体育館' },
+    { value: '7', label: '工学部1号館' },
+    { value: '11', label: '工学部2号館' },
+    { value: '11', label: '工学部3号館' },
+    { value: '13', label: '工学部4号館' },
+    { value: '14', label: '工学部5号館' },
+    { value: '15', label: '工学部6号館' },
+    { value: '16', label: '工学部7号館' },
+    { value: '17', label: '工学部8号館' },
+    { value: '18', label: '武道館' },
+    { value: '18', label: '佐鳴会館' },
+    { value: '20', label: '課外活動共用施設' },
+    // { value: '21', label: 'ものづくり館' },
+    // { value: '22', label: '次世代ものづくり人材育成センター' },
+    { value: '23', label: 'イノベーション社会連携推進機構' },
+    { value: '6', label: '情報基盤センター' },
+    { value: '6', label: '創造科学技術大学院' },
+    // { value: '26', label: '光創起イノベーション研究拠点棟' },
+    // { value: '27', label: '電子工学研究所' },
+    // { value: '28', label: 'ナノデバイス作製・評価センター' },
+    // { value: '29', label: '高柳記念未来技術創造館' },
+    { value: '17', label: 'プール' },
+    { value: '15', label: '弓道場' },
+    { value: '32', label: 'テニスコート'},
+  ];
+
+  const [overlayImages, setOverlayImages] = useState<string[]>([]);
+
+  const [option1, setOption1] = useState<string>("");
+  const [option2, setOption2] = useState<string>("");
+
+  // ダイクストラ結果を表示
+  const [distance, setDistance] = useState<number | null>(null);
+  const [pathNodes, setPathNodes] = useState<string[]>([]);
+  const [pathEdges, setPathEdges] = useState<string[]>([]);
+
+  const handleOption1Change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setOption1(e.target.value);
+  };
+  const handleOption2Change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setOption2(e.target.value);
+  };
+
+  // Button1
+  const handleButton1Click = () => {
+    // option1, option2 が空の場合は処理しない例
+    if (!option1 || !option2) {
+      alert("Please select both start and end nodes!");
+      return;
+    }
+    // ダイクストラ実行
+    const result = dijkstra(graphRelations, option1, option2);
+    setDistance(result.distance);
+    setPathNodes(result.pathNodes);
+    setPathEdges(result.pathEdges);
+
+    // ラベルの画像を表示
+    const edgeImages = result.pathEdges.map((label) => images[`${label}.png`]);
+    setOverlayImages(edgeImages);
+  };
+
+  // Button2
+  const handleButton2Click = () => {
+    setOverlayImages([images['map.png'], images['difsize.png']]);
+  };
+
+  return (
+    <div className="container">
+      <div className="image-area">
+        {overlayImages.map((imgSrc, index) => (
+          <img
+            key={index}
+            src={imgSrc}
+            alt={`Layer ${index}`}
+            className="overlay-image"
+          />
+        ))}
+      </div>
+
+      {/* 右カラム */}
+      <div className="right-column">
+        <h1 className="title">Fresh Navi</h1>
+        <h2 className="subtitle">出発地</h2>
+        {/* 選択メニュー1 */}
+        <select className="blue-area" value={option1} onChange={handleOption1Change}>
+          <option value="">--Select--</option>
+          {buildingSelection.map((opt) => (
+            <option key={opt.value} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
+
+        <h2 className="subtitle">目的地</h2>
+        {/* 選択メニュー2 */}
+        <select className="blue-area" value={option2} onChange={handleOption2Change}>
+          <option value="">--Select--</option>
+          {buildingSelection.map((opt) => (
+            <option key={opt.value} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
+
+        {/* ボタン */}
+        <button className="green-button" onClick={handleButton1Click}>
+          経路を検索
+        </button>
+        <button className="green-button" onClick={handleButton2Click}>
+          Button2
+        </button>
+
+        {/* 結果表示 debug用 */}
+        {/* {distance !== null && ( */}
+        {/*   <div style={{ marginTop: "1rem" }}> */}
+        {/*     <p>最短距離: {distance}</p> */}
+        {/*     <p>通過ノード: {pathNodes.join(" → ")}</p> */}
+        {/*     <p>通過ラベル: {pathEdges.join(" → ")}</p> */}
+        {/*   </div> */}
+        {/* )} */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Graph.tsx
@@ -0,0 +1,265 @@
+// 辺
+export type Edge = {
+  to: string;       // 到達先ノード
+  cost: number;     // 距離
+  label: string;    // ラベル
+};
+
+// グラフ構築
+export type Graph = {
+  [nodeId: string]: Edge[];
+};
+
+// グラフ
+export const graphRelations: Graph = {
+  '32': [
+    { to: '59', cost: 18, label: '20' },
+    { to: '23', cost: 23, label: '22' },
+  ],
+  '23': [
+    { to: '32', cost: 23, label: '22' },
+    { to: '14', cost: 8, label: '47' },
+  ],
+  '59': [
+    { to: '32', cost: 18, label: '20' },
+    { to: '58', cost: 10, label: '19' },
+    { to: '60', cost: 24, label: '17' },
+  ],
+  '60': [
+    { to: '59', cost: 24, label: '17' },
+    { to: '66', cost: 10, label: '15' },
+  ],
+  '8': [
+    { to: '58', cost: 10, label: '21' },
+    { to: '14', cost: 13, label: '23' },
+  ],
+  '58': [
+    { to: '8', cost: 10, label: '21' },
+    { to: '59', cost: 10, label: '19' },
+    { to: '7', cost: 17, label: '18' },
+    { to: '56', cost: 13, label: '24' },
+  ],
+  '7': [
+    { to: '58', cost: 17, label: '18' },
+    { to: '11', cost: 7, label: '25' },
+    { to: '66', cost: 7, label: '16' },
+  ],
+  '66': [
+    { to: '7', cost: 7, label: '16' },
+    { to: '60', cost: 10, label: '15' },
+    { to: '13', cost: 4, label: '14' },
+  ],
+  '13': [
+    { to: '66', cost: 4, label: '14' },
+    { to: '15', cost: 8, label: '13' },
+    { to: '6', cost: 13, label: '12' },
+  ],
+  '15': [
+    { to: '13', cost: 8, label: '13' },
+  ],
+  '11': [
+    { to: '7', cost: 7, label: '25' },
+    { to: '55', cost: 6, label: '26' },
+  ],
+  '14': [
+    { to: '23', cost: 8, label: '47' },
+    { to: '8', cost: 13, label: '23' },
+    { to: '57', cost: 7, label: '41' },
+    { to: '18', cost: 6, label: '45' },
+  ],
+  '57': [
+    { to: '14', cost: 7, label: '41' },
+    { to: '3', cost: 6, label: '39' },
+    { to: '56', cost: 3, label: '37' },
+  ],
+  '56': [
+    { to: '57', cost: 3, label: '37' },
+    { to: '58', cost: 13, label: '24' },
+    { to: '65', cost: 5, label: '34' },
+    { to: '64', cost: 6, label: '35' },
+  ],
+  '65': [
+    { to: '56', cost: 5, label: '34' },
+    { to: '62', cost: 6, label: '32' },
+    { to: '55', cost: 12, label: '27' },
+  ],
+  '55': [
+    { to: '6', cost: 11, label: '11' },
+    { to: '65', cost: 12, label: '27' },
+    { to: '11', cost: 6, label: '26' },
+    { to: '52', cost: 7, label: '4' },
+  ],
+  '6': [
+    { to: '55', cost: 11, label: '11' },
+    { to: '13', cost: 13, label: '12' },
+    { to: '4', cost: 2, label: '10' },
+  ],
+  '4': [
+    { to: '6', cost: 2, label: '10' },
+    { to: '5', cost: 4, label: '8' },
+    { to: '53', cost: 5, label: '9' },
+  ],
+  '5': [
+    { to: '4', cost: 4, label: '8' },
+    { to: '2', cost: 5, label: '7' },
+  ],
+  '2': [
+    { to: '5', cost: 5, label: '7' },
+    { to: '53', cost: 4, label: '6' },
+  ],
+  '53': [
+    { to: '4', cost: 5, label: '9' },
+    { to: '2', cost: 4, label: '6' },
+    { to: '52', cost: 11, label: '5' },
+  ],
+  '52': [
+    { to: '55', cost: 7, label: '4' },
+    { to: '53', cost: 11, label: '5' },
+    { to: '51', cost: 3, label: '3' },
+  ],
+  '51': [
+    { to: '52', cost: 3, label: '3' },
+    { to: '1', cost: 4, label: '2' },
+    { to: '50', cost: 3, label: '1' },
+  ],
+  '1': [
+    { to: '51', cost: 4, label: '2' },
+  ],
+  '50': [
+    { to: '51', cost: 3, label: '1' },
+    { to: '63', cost: 11, label: '28' },
+  ],
+  '62': [
+    { to: '65', cost: 6, label: '32' },
+    { to: '64', cost: 6, label: '33' },
+    { to: '63', cost: 7, label: '31' },
+  ],
+  '63': [
+    { to: '62', cost: 7, label: '31' },
+    { to: '50', cost: 11, label: '28' },
+    { to: '16', cost: 3, label: '29' },
+  ],
+  '16': [
+    { to: '63', cost: 3, label: '29' },
+    { to: '61', cost: 3, label: '30' },
+  ],
+  '64': [
+    { to: '56', cost: 6, label: '35' },
+    { to: '62', cost: 6, label: '33' },
+    { to: '61', cost: 7, label: '36' },
+  ],
+  '61': [
+    { to: '16', cost: 3, label: '30' },
+    { to: '64', cost: 7, label: '36' },
+    { to: '17', cost: 3, label: '38' },
+  ],
+  '3': [
+    { to: '57', cost: 6, label: '39' },
+    { to: '17', cost: 7, label: '40' },
+    { to: '18', cost: 7, label: '43' },
+  ],
+  '17': [
+    { to: '61', cost: 3, label: '38' },
+    { to: '3', cost: 7, label: '40' },
+    { to: '54', cost: 7, label: '42' },
+  ],
+  '18': [
+    { to: '14', cost: 6, label: '45' },
+    { to: '3', cost: 7, label: '43' },
+    { to: '54', cost: 7, label: '44' },
+    { to: '20', cost: 6, label: '46' },
+  ],
+  '54': [
+    { to: '18', cost: 7, label: '44' },
+    { to: '17', cost: 7, label: '42' },
+  ],
+  '20': [
+    { to: '18', cost: 6, label: '46' },
+  ],
+};
+
+export function dijkstra(graph: Graph, start: string, end: string): { distance: number; pathNodes: string[]; pathEdges: string[] } {
+  // distance  最短距離
+  // pathNodes 経路のノード
+  // pathEdges 経路のラベル
+
+  // 各ノードへの最短距離を保持(初期値: Infinity)
+  const dist: { [node: string]: number } = {};
+  // 経路記録用
+  const prevNode: { [node: string]: string | null } = {};
+  const prevEdge: { [node: string]: string | null } = {};
+
+  // 全ノード
+  const allNodes = Object.keys(graph);
+
+  // 初期化
+  allNodes.forEach((node) => {
+    dist[node] = Infinity;
+    prevNode[node] = null;
+    prevEdge[node] = null;
+  });
+  dist[start] = 0;
+
+  // 未訪問ノード
+  const unvisited = new Set(allNodes);
+
+  while (unvisited.size > 0) {
+    // 未訪問ノードの中のdistが最小のノードを探す
+    let current: string | null = null;
+    let minDistance = Infinity;
+
+    for (const node of unvisited) {
+      if (dist[node] < minDistance) {
+        minDistance = dist[node];
+        current = node;
+      }
+    }
+
+    if (current === null) {
+      // これ以上更新できるノードがない場合
+      break;
+    }
+
+    if (current === end) {
+      break;
+    }
+
+    // 探索が終わったら unvisited から除外
+    unvisited.delete(current);
+
+    // 隣接するノードの距離を更新
+    for (const edge of graph[current]) {
+      const alt = dist[current] + edge.cost;
+      if (alt < dist[edge.to]) {
+        dist[edge.to] = alt;
+        prevNode[edge.to] = current;
+        prevEdge[edge.to] = edge.label; // どの辺を辿ったか記憶
+      }
+    }
+  }
+
+  // 経路復元
+  const pathNodes: string[] = [];
+  const pathEdges: string[] = [];
+  let node: string | null = end;
+
+  while (node) {
+    pathNodes.unshift(node);
+    const label = prevEdge[node];
+    if (label) {
+      pathEdges.unshift(label);
+    }
+    node = prevNode[node];
+  }
+
+  // pathNodes[0]がstartじゃなく、経路が存在しない場合
+  if (pathNodes[0] !== start) {
+    return { distance: Infinity, pathNodes: [], pathEdges: [] };
+  }
+
+  return {
+    distance: dist[end],
+    pathNodes,
+    pathEdges,
+  };
+}
